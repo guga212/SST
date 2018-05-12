7ae6b73a530a262fbd17b846bab4b9d5fa3c234d
guga212 <34397511+guga212@users.noreply.github.com>
2018-05-12 10:55:26 +0200

    Changed decison variables from velocity to aceleration.

--- MySynchSimulationTimeHorizont.m
+++ MySynchSimulationTimeHorizont.m
@@ -17,12 +17,13 @@ function simpleTest()
         vrep.simxStartSimulation(clientID,vrep.simx_opmode_blocking);
 
         % Now step a few times:
-        N=8;
+        N=4;
         sim_dt=0.05;
         sim_t=0;
         idx=1;
         t=0.05;
-        v_0=zeros(N,2,t/sim_dt);   
+        a_0=zeros(N,2,t/sim_dt);   
+        v_0=zeros(N,2);   
         
         while(1)
             
@@ -30,9 +31,9 @@ function simpleTest()
                 [main_trgt_posd, main_trgt_pos]=GetDisturbedMainTrgtPosition(vrep,clientID);
                 [xd_0, x_0]=GetDisturbedQuadPosition(vrep,clientID,N);
              
-                %[trgt_alg_x, trgt_alg_y]=OptimizeNextMove( main_trgt_pos(1:2), [1.41;1.41;1.41;1.41 ].*1.0, [0 2 2.828 2; 2 0 2 2.828; 2.828 2 0 2; 2 2.828 2 0].*1.0, v_0,x_0(:,1:2),t,1.2, 1.2, 1, 6);
-                [trgt_alg_x, trgt_alg_y]=OptimizeNextMove( main_trgt_pos(1:2), [1.41;1;1.41;1;1.41;1;1.41;1].*1.8, [0 1 2 2.236 2.828 2.236 2 1; 1 0 1 1.41 2.236 2 2.236 1.41;  2 1 0 1 2 2.236 2.828 2.236; 2.236 1.41 1 0 1 1.41 2.236 2; 2.828 2.236 2 1 0 1 2 2.236; 2.828 2 2.236 1.41 1 0 1 1.41; 2 2.236 2.828 2.236 2 1 0 1; 1 1.41 2.236 2 2.236 1.41 1 0; ].*1.8, v_0,x_0(:,1:2),t,1.4, 1.6, 1, 6);
-                v_0=[trgt_alg_x(:,end), trgt_alg_y(:,end)];
+                [trgt_alg_x, trgt_alg_y]=OptimizeNextMove( main_trgt_pos(1:2), [1.41;1.41;1.41;1.41 ].*1.0, [0 2 2.828 2; 2 0 2 2.828; 2.828 2 0 2; 2 2.828 2 0].*1.0,a_0,v_0,x_0(:,1:2),t,1.2, 1.2, 1, 6);
+                %[trgt_alg_x, trgt_alg_y]=OptimizeNextMove( main_trgt_pos(1:2), [1.41;1;1.41;1;1.41;1;1.41;1].*1.8, [0 1 2 2.236 2.828 2.236 2 1; 1 0 1 1.41 2.236 2 2.236 1.41;  2 1 0 1 2 2.236 2.828 2.236; 2.236 1.41 1 0 1 1.41 2.236 2; 2.828 2.236 2 1 0 1 2 2.236; 2.828 2 2.236 1.41 1 0 1 1.41; 2 2.236 2.828 2.236 2 1 0 1; 1 1.41 2.236 2 2.236 1.41 1 0; ].*1.8, v_0,x_0(:,1:2),t,1.4, 1.6, 1, 6);
+                a_0=[trgt_alg_x(:,end), trgt_alg_y(:,end)];               
                 sim_t=t;
                 idx=1;
                 posX=squeeze(x_0(:,1));
@@ -40,14 +41,17 @@ function simpleTest()
             end
             
             for i=0:N-1
-                posX(i+1)=posX(i+1)+sim_dt*trgt_alg_x(i+1,idx);
-                posY(i+1)=posY(i+1)+sim_dt*trgt_alg_y(i+1,idx);
+                posX(i+1)=posX(i+1)+sim_dt*v_0(i+1,1)+0.5*trgt_alg_x(i+1,idx)*sim_dt^2;
+                posY(i+1)=posY(i+1)+sim_dt*v_0(i+1,2)+0.5*trgt_alg_y(i+1,idx)*sim_dt^2;
                 SetQuadTrgtPos(vrep, clientID, i, [posX(i+1) posY(i+1) x_0(i+1,3)]);                                
             end
             
             vrep.simxSynchronousTrigger(clientID);
             sim_t=sim_t-sim_dt;
+            v_0(:,1)=v_0(:,1)+trgt_alg_x(:,idx)*sim_dt;
+            v_0(:,2)=v_0(:,2)+trgt_alg_y(:,idx)*sim_dt;
             idx=idx+1;
+            
         end
 
         % stop the simulation:
@@ -79,7 +83,7 @@ function [dpos, pos]=GetDisturbedQuadPosition(VrepAPI, ClientID, size)
 end
 
 
-%x zmienne pozycji 
+ 
 %X zmienna celu
 %D zadana odleglosc do celu 
 %d zadana macierz odleglosc do sasiada
@@ -90,50 +94,64 @@ end
 %d_min minimalna odleglosc do sasiada
 %V_max maksymalna predkosc
 %a_max maksymalne przyspieszenie
-function [Vx,Vy]=OptimizeNextMove(X,D,d,v_0,x_0,t,D_min,d_min,V_max,a_max)
-    tfun = @(v)TargetFunction(v,X,D,d,x_0,t);
-    %[res, minval]=fminunc(tfun, v_0);
+function [Vx,Vy]=OptimizeNextMove(X,D,d,a_0,v_0,x_0,t,D_min,d_min,V_max,a_max)
+    tfun = @(a)TargetFunction(a,X,D,d,v_0,x_0,t);
+   %[res, minval]=fminunc(tfun, a_0);
 
     
-    cfun=@(v)ConstraintFunction(v,X,D_min,d_min,x_0,v_0,t,V_max,a_max);
+    cfun=@(a)ConstraintFunction(a,X,D_min,d_min,a_0,v_0,x_0,t,V_max,a_max);
     %options = optimoptions('fmincon','Display','off');
-    options = optimoptions('fmincon','MaxFunctionEvaluations',12000,'MaxIterations', 1600);
-    [res, minval]=fmincon(tfun,v_0,[],[],[],[],[],[],cfun,options);
-  
+    %options = optimoptions('fmincon','Algorithm','sqp','MaxFunctionEvaluations',24000,'MaxIterations', 3200);
+    options = optimoptions('fmincon','Algorithm','sqp', 'SpecifyObjectiveGradient',false);
+    [res, minval,exitflag,output,lambda,grad]=fmincon(tfun,a_0,[],[],[],[],[],[],cfun,options);
+
     Vx=squeeze(res(:,1,:));
     Vy=squeeze(res(:,2,:));
 end
 
-%x zmienne pozycji 
+%a zmienne przysp 
 %X zmienna celu
 %D zadana odleglosc do celu 
 %d zadana macierz odleglosc do sasiada
 %v_0 predkosc poczatkowa
 %x_0 pozycja poczatkowa
 %t horyzont predykcji
-function out=TargetFunction(v,X,D,d,x_0,t)
-
-    out=0;
-    dt=t/size(v,3);
+function [out,grad]=TargetFunction(a,X,D,d,v_0,x_0,t)
     
-    %Calculate final position
-    for i=1:size(v,1)        
-        x(i,1)=x_0(i,1);
-        x(i,2)=x_0(i,2);        
-        for j=1:size(v,3)
-            x(i,1)=x(i,1)+v(i,1,j)*dt;
-            x(i,2)=x(i,2)+v(i,2,j)*dt;
+    out=0;
+    dt=t/size(a,3);
+    
+    %Calculate all velocity of trajectory
+    for i=1:size(a,1)
+        v(i,1,1)=v_0(i,1)+a(i,1,1)*dt;
+        v(i,2,1)=v_0(i,2)+a(i,2,1)*dt;
+        for j=2:size(a,3)
+            v(i,1,j)=v(i,1,j-1)+a(i,1,j)*dt;
+            v(i,2,j)=v(i,2,j-1)+a(i,2,j)*dt;
         end
     end
     
+    %Calculate final position
+    for i=1:size(a,1)        
+      
+        x(i,1)=x_0(i,1)+(v_0(i,1)+v(i,1,1))*0.5*dt;
+        x(i,2)=x_0(i,2)+(v_0(i,2)+v(i,2,1))*0.5*dt;
+        
+        for j=2:size(a,3)            
+                x(i,1)=x(i,1)+(v(i,1,j-1)+v(i,1,j))*0.5*dt;
+                x(i,2)=x(i,2)+(v(i,2,j-1)+v(i,2,j))*0.5*dt;
+        end
+        
+    end
+    
     %Minimize distance from main target in the final point
-    for i=1:size(v,1)        
+    for i=1:size(a,1)        
         out=out+(D(i)^2-(X(1,1)-x(i,1))^2-(X(1,2)-x(i,2))^2)^2;
     end
     
     %Minimize formation error in the final point
-    for i=1:size(v,1)       
-        for j=1:size(v,1)            
+    for i=1:size(a,1)       
+        for j=1:size(a,1)            
             weight=1.0;         
             out=out+weight*(d(i,j)^2-(x(i,1)-x(j,1))^2-(x(i,2)-x(j,2))^2)^2;                    
         end
@@ -146,10 +164,60 @@ function out=TargetFunction(v,X,D,d,x_0,t)
             out=out+weight*(v(i,1,j)^2+v(i,2,j)^2);
         end
     end
+    
+    %Gradient required
+    if nargout > 1 
+        for i=1:size(v,1)
+           for j=1:size(v,3)
+               
+               % Gradient minimize distance from main trgt
+               grad1(i,1,j)=0.5*dt^2;
+               grad1(i,2,j)=0.5*dt^2;
+               for k=1:size(v,3)-1
+                   grad1(i,1,j)=grad1(i,1,j)+dt;
+                   grad1(i,2,j)=grad1(i,2,j)+dt;
+               end
+               grad1(i,1,j)=2*(D(i)^2-(X(1,1)-x(i,1))^2)*2*(X(1,1)-x(i,1))*-grad1(i,1,j);
+               grad1(i,2,j)=2*(D(i)^2-(X(1,2)-x(i,2))^2)*2*(X(1,2)-x(i,2))*-grad1(i,2,j);
+               
+               
+               % Gradient minimize formation error
+               grad2(i,1,j)=0;
+               grad2(i,2,j)=0;
+               
+               for n=1:size(a,1)               
+                   grad2(i,1,j)=grad2(i,1,j)+0.5*dt^2;
+                   grad2(i,2,j)=grad2(i,2,j)+0.5*dt^2;
+                   for k=1:size(v,3)-1
+                       grad2(i,1,j)=grad2(i,1,j)+dt;
+                       grad2(i,2,j)=grad2(i,2,j)+dt;
+                   end               
+                   grad2(i,1,j)=2*weight*(d(i,n)^2-(x(i,1)-x(n,1))^2)*2*(x(i,1)-x(n,1))*-grad2(i,1,j);
+                   grad2(i,2,j)=2*weight*(d(i,n)^2-(x(i,2)-x(n,2))^2)*2*(x(i,2)-x(n,2))*-grad2(i,1,j);
+               end
+               
+               
+               
+               % Gradient minimize formation error
+               grad3(i,1,j)=0;
+               grad3(i,2,j)=0;
+                 for k=1:size(v,3)-1
+                   grad3(i,1,j)=grad3(i,1,j)+dt;
+                   grad3(i,2,j)=grad3(i,2,j)+dt;
+                 end
+               grad3(i,1,j)=2*weight*v(i,1,j)*grad3(i,1,j);
+               grad3(i,2,j)=2*weight*v(i,2,j)*grad3(i,2,j);
+               
+               grad(i,1,j)=grad1(i,1,j)+grad2(i,1,j)+grad3(i,1,j);
+               grad(i,2,j)=grad1(i,2,j)+grad2(i,2,j)+grad3(i,2,j);
+           end           
+        end
+    end
+    
 end
 
 
-%x zmienne pozycji 
+%a zmienne przysp
 %X zmienna celu
 %D_min minimalna odleglosc do celu
 %d_min minimalna odleglosc do sasiada
@@ -157,17 +225,27 @@ end
 %t horyzont predykcji
 %V_max maksymalna predkosc
 %a_max maksymalne przyspieszenie
-function [c,ceq, gradc, gradceq]=ConstraintFunction(v,X,D_min,d_min,x_0,v_0,t,V_max,a_max)
+function [c,ceq, gradc, gradceq]=ConstraintFunction(a,X,D_min,d_min,a_0,v_0,x_0,t,V_max,a_max)
     
-    dt=t/size(v,3);
+    dt=t/size(a,3);
+    
+    %Calculate all velocity of trajectory
+    for i=1:size(a,1)
+        v(i,1,1)=v_0(i,1)+a(i,1,1)*dt;
+        v(i,2,1)=v_0(i,2)+a(i,2,1)*dt;
+        for j=2:size(a,3)
+            v(i,1,j)=v(i,1,j-1)+a(i,1,j)*dt;
+            v(i,2,j)=v(i,2,j-1)+a(i,2,j)*dt;
+        end
+    end
     
     %Calculate all positions of trajectory
-    for i=1:size(v,1)        
-        x(i,1,1)=x_0(i,1)+v(i,1,1)*dt;
-        x(i,2,1)=x_0(i,2)+v(i,2,1)*dt;        
+    for i=1:size(a,1)        
+        x(i,1,1)=x_0(i,1)+0.5*(v_0(i,1)+v(i,1,1))*dt;
+        x(i,2,1)=x_0(i,2)+0.5*(v_0(i,2)+v(i,2,1))*dt;
         for j=2:size(v,3)
-            x(i,1,j)=x(i,1,j-1)+v(i,1,j)*dt;
-            x(i,2,j)=x(i,2,j-1)+v(i,2,j)*dt;
+            x(i,1,j)=x(i,1,j-1)+0.5*(v(i,1,j-1)+v(i,1,j))*dt;
+            x(i,2,j)=x(i,2,j-1)+0.5*(v(i,2,j-1)+v(i,2,j))*dt;
         end
     end
 
@@ -183,7 +261,7 @@ function [c,ceq, gradc, gradceq]=ConstraintFunction(v,X,D_min,d_min,x_0,v_0,t,V_
             c(cur_ind)=D_min^2-(X(1,1)-x(i,1,j))^2-(X(1,2)-x(i,2,j))^2;
         end
     end
-    
+     
     %Minimal distance from other vehicles in every position
     for k=1:size(x,3)
     
@@ -207,14 +285,12 @@ function [c,ceq, gradc, gradceq]=ConstraintFunction(v,X,D_min,d_min,x_0,v_0,t,V_
             c(cur_ind)=v(i,1,k)^2+v(i,2,k)^2-V_max^2;
         end
     end
-    
+     
     %Maximal acceleration
-    for i=1:size(v,1)
-        cur_ind=cur_ind+1;          
-        c(cur_ind)=(v_0(i,1)-v(i,1,1))^2+(v_0(i,2)-v(i,2,1))^2-(dt*a_max)^2;
-        for j=2:size(v,3)
+    for i=1:size(a,1)
+        for j=1:size(a,3)
             cur_ind=cur_ind+1;          
-            c(cur_ind)=(v(i,1,j-1)-v(i,1,j))^2+(v(i,2,j-1)-v(i,2,j))^2-(dt*a_max)^2;
+            c(cur_ind)=a(i,1,j)^2+a(i,2,j)^2-a_max^2;
         end
     end
     
