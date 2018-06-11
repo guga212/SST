62a0297891101b161906d8b54453192bc0f0bf5c
guga212 <34397511+guga212@users.noreply.github.com>
2018-06-11 21:49:23 +0200

    Added functionality for presentation

--- AppForControl/GetDisturbedMainTrgtPosition.m
+++ AppForControl/GetDisturbedMainTrgtPosition.m
@@ -1,5 +1,5 @@
-function [dpos, pos]=GetDisturbedMainTrgtPosition(VrepAPI, ClientID, scale)
+function [dpos, pos]=GetDisturbedMainTrgtPosition(VrepAPI, ClientID, scale, name)
     dpos=zeros(1,3);
-    pos=GetMainTrgtPos(VrepAPI, ClientID);
+    pos=GetMainTrgtPos(VrepAPI, ClientID,name);
     dpos(1,:)=pos+(rand(1,3)-ones(1,3)).*pos*(scale/100);
 end
--- AppForControl/GetMainTrgtPos.m
+++ AppForControl/GetMainTrgtPos.m
@@ -1,9 +1,7 @@
-function Position = GetMainTrgtPos(VrepAPI, ClientID)
-    MainTrgtName='Bill_base';
-    persistent MainTrgtHandle;
-    if isempty(MainTrgtHandle)
-        [return_code, MainTrgtHandle]=VrepAPI.simxGetObjectHandle(ClientID, MainTrgtName, VrepAPI.simx_opmode_blocking);
-    end
+function Position = GetMainTrgtPos(VrepAPI, ClientID,name)
+    %MainTrgtName='Bill_base';
+    MainTrgtName=name;        
+    [return_code, MainTrgtHandle]=VrepAPI.simxGetObjectHandle(ClientID, MainTrgtName, VrepAPI.simx_opmode_blocking);   
     [return_code, Position]=VrepAPI.simxGetObjectPosition (ClientID, MainTrgtHandle, -1, VrepAPI.simx_opmode_blocking);
     Position=double(Position);
 end
